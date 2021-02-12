c3a2b2955243bfca3c54c8dd6a4f865d5b35f434
Jaeyun Kim <ktwin01@icloud.com>
2021-02-13 00:09:04 +0900

    update

--- 2655/wombats.cpp
+++ 2655/wombats.cpp
@@ -1,31 +1,56 @@
 #include <cstdio>
 using namespace std;
 
-class seg_tree{
+class SegTree{
 public:
-	
-};
-int R, C;
-int row_wombat[5050][220], col_wombat[220][5050];
+	int R, C;
+	int row_wombat[5050][220], col_wombat[5050][220];	
+	int tree[20020][220][220];
 
-void input(){
-	scanf("%d %d", &R, &C);
+	void init(int now, int r, int l){
+
+	}
+
+	void update(int now, int r, int l, int s, int e){
+		if(r > e || l < s)
+			return;
+		if(r == l){
+			// caculate r == l
+			return;
+		}
+		int m = (r+l)/2;
+		update(now*2, r, m, s, e);
+		update(now*2+1, m, l, s, e);
+		// caculate r, m, l
+
+	}
+
+	int get_ans(int a, int b){
+		return tree[1][a][b];
+	}
+};
+
+SegTree tree;
+
+
+void input(SegTree & tree){
+	scanf("%d %d", &tree.R, &tree.C);
 	
-	for(int i=0; i<R; i++){
-		for(int j=1; j<C; j++){
-			scanf("%d", row_wombat[i]+j);
+	for(int i=1; i<= tree.R; i++){
+		for(int j=1; j<tree.C; j++){
+			scanf("%d", tree.row_wombat[i]+j);
 		}
 	}
-	for(int j=0; j<R-1; j++){
-		for(int i=0; i<C; i++){
-			scanf("%d", col_wombat[i]+j);
+	for(int j=1; j<= tree.R-1; j++){
+		for(int i=1; i<=tree.C; i++){
+			scanf("%d", tree.col_wombat[i]+j);
 		}
 	}
+
+	tree.init(1, 1, tree.R);
 }
 
-
-
-void solve(){
+void solve(SegTree & tree){
 	int E;
 	scanf("%d", &E);
 	while(E--){
@@ -36,23 +61,22 @@ void solve(){
 			int p, q, w;
 			scanf("%d %d %d", &p, &q, &w);
 			if(type == 1){
-				
+				tree.row_wombat[p][q] = w;				
 			}else{
-
+				tree.col_wombat[p][q] = w;
 			}
-
 		}else{
 			int v1, v2;
 			scanf("%d %d", &v1, &v2);
-
-
+			printf("%d\n", tree.get_ans(v1, v2));
 		}
 	}
 
 }
+
 int main(){
-	input();
-	solve();
+	input(tree);
+	solve(tree);
 	
 	return 0;
 }
